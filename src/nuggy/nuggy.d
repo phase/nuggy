47a555ed8e6a532d09d170f067de511c595fcdb5
phase <ph4sesaber@gmail.com>
2016-01-16 01:14:04 -0800

    Switch to D

---
+++ src/nuggy/nuggy.d
@@ -0,0 +1,33 @@
+module nuggy;
+import std.file;
+import std.base64;
+import std.json;
+import std.algorithm;
+import std.string;
+import std.conv;
+import std.socket;
+import core.stdc.stdlib;
+
+string VERSION = "1.8-0.0.1"; ///Version of Nuggy
+string HOSTNAME = "localhost";
+ushort PORT = 25565;
+
+void parseArguments(char[][] args) {
+    foreach(w; args) {
+        if(w == "--debug" || w == "-d") {
+            //setDebug(true);
+        }
+        else if(w.startsWith("-h=")){
+            HOSTNAME = (to!string(w).split("-h="))[1];
+        }
+        else if(w.startsWith("-p=")){
+            PORT = to!ushort((to!string(w).split("-p="))[1]);
+        }
+    }
+}
+
+int main(char[][] args) {
+    writeln("\n Nuggy v" ~ VERSION ~ " initializing...\n");
+    parseArguments(args);
+    return 0;
+}
